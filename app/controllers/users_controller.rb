4a48a5f63f09263f0cdcc49c1657394ff63447f5
Markus Wust <markuswust@yahoo.com>
2012-02-24 15:44:13 -0500

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -43,15 +43,13 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
+  
+    def show
+      @user = User.find(params[:id])
+      @microposts = @user.microposts.paginate(:page => params[:page])
+    end
 
   private
-  
-    def signed_in_user
-      unless signed_in?
-        store_location
-        redirect_to signin_path, :notice => "Please sign in."
-      end
-    end  
     
     def correct_user
       @user = User.find(params[:id])
@@ -65,5 +63,4 @@ class UsersController < ApplicationController
     def admin_user
       redirect_to(root_path) unless current_user.admin?
     end
-  
 end
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,7 +1,16 @@
 FactoryGirl.define do
   factory :user do
-    name     "Michael Hartl"
-    email    "michael@example.com"
+    sequence(:name)  { |n| "Person #{n}" }
+    sequence(:email) { |n| "person_#{n}@example.com"}   
     password "foobar"
+
+    factory :admin do
+      admin true
+    end
+  end
+
+  factory :micropost do
+    content "Lorem ipsum"
+    user
   end
 end
